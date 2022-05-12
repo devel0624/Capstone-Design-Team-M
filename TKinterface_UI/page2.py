4a003e2227c977fd70a6fd2d4a428e8803a38172
pingu52 <81621996+pingu52@users.noreply.github.com>
2022-05-13 02:23:46 +0900

    Tkinter_UI
    Tkinter_UI 20174188_김형균

--- TKinterface_UI/page2.py
+++ TKinterface_UI/page2.py
@@ -6,12 +6,15 @@ from PIL import Image, ImageTk
 import cv2 as cv # OpenCV
 import os
 import ctypes
+import webbrowser
 
 ws = Tk()
 ws.geometry('400x300')
 ws.title('SELECT PLATFORM')
 ws['bg']='#ffbf00'
 
+
+youtube = cv.imread("F:/Capstone-Design-Team-M-main/img/그림/youtube.png")
 f = ("Times bold", 14)
  
 def quit_window(icon, item):
@@ -26,8 +29,8 @@ def hide_window():
     if messagebox.askyesno("Quit", "트레이로 최소화 하시겠습니까?"):
         ws.withdraw()
         image=Image.open("C:/Users/gudrb/Desktop/그리;ㅁ/곰곰.png")
-        menu=(item('Quit', quit_window), item('Show', show_window))
-        icon=pystray.Icon("name", image, "My System Tray Icon", menu)
+        menu=(item('종료', quit_window), item('열기', show_window))
+        icon=pystray.Icon("name", image, "Gesture Program", menu)
         icon.run()
     else:
         ws.destroy()
@@ -40,9 +43,17 @@ def prevPage():
     ws.destroy()
     import page1
 
+def Netflix_b():
+    hide_window
+    webbrowser.open("https://www.netflix.com/kr/")
+
+def Youtube_b():
+    hide_window
+    webbrowser.open("https://www.youtube.com/")
+
 Label(
     ws,
-    text="This is Second page",
+    text="NETFLIX YOUTUBE",
     padx=20,
     pady=20,
     bg='#ffbf00',
@@ -51,16 +62,18 @@ Label(
 
 Button(
     ws, 
-    text="나가기", 
+    text="YOUTUBE", 
     font=f,
-    command=hide_window
+    command=Youtube_b
+    #image=youtube
+    
     ).pack(fill=X, expand=TRUE, side=LEFT)
 Button(
     ws, 
-    text="다음", 
+    text="NETFLIX", 
     font=f,
-    command=nextPage
-    ).pack(fill=X, expand=TRUE, side=LEFT)
+    command=Netflix_b
+    ).pack(fill=X, expand=TRUE, side=RIGHT)
 
 
 ws.protocol('WM_DELETE_WINDOW', hide_window)
